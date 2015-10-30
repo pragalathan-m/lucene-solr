94dd910999710a21bad71fb9cba3214b68f6d756
Vivek Mittal <vivek.mittal@flipkart.com>
2015-10-30 17:06:23 +0530

    Adding code to push to artifactory

---
+++ maven-build/push_to_artifactory.sh
@@ -0,0 +1,2 @@
+mvn clean deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
+mvn clean deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
